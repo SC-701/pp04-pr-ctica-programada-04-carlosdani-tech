d34d3a00bf665d09291c1b140dadf5bc3d793008
Carlos Gutiérrez <cdaniguti04@gmail.com>
2026-03-22 23:53:30 -0600

    ajuste API

--- Vehiculo.API/API/Controllers/VehiculoController.cs
+++ Vehiculo.API/API/Controllers/VehiculoController.cs
@@ -1,12 +1,14 @@
 ﻿using Abstracciones.Interfaces.API;
 using Abstracciones.Interfaces.Flujo;
 using Abstracciones.Modelos;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class VehiculoController : ControllerBase, IVehiculoController
     {
         public IVehiculoFlujo _vehiculoFlujo;
@@ -19,6 +21,7 @@ namespace API.Controllers
         }
 
         [HttpPost]
+        [Authorize(Roles = "2")]
         public async Task<IActionResult> Agregar([FromBody]VehiculoRequest vehiculo)
         {
             var resultado = await _vehiculoFlujo.Agregar(vehiculo);
@@ -26,6 +29,7 @@ namespace API.Controllers
         }
 
         [HttpPut("{Id}")]
+        [Authorize(Roles = "2")]
         public async Task<IActionResult> Editar([FromRoute]Guid Id, [FromBody]VehiculoRequest vehiculo)
         {
             if (!await VerificarVehiculoExiste(Id))
@@ -35,6 +39,7 @@ namespace API.Controllers
         }
 
         [HttpDelete("{Id}")]
+        [Authorize(Roles = "2")]
         public async Task<IActionResult> Eliminar([FromRoute]Guid Id)
         {
             if (!await VerificarVehiculoExiste(Id))
@@ -44,6 +49,7 @@ namespace API.Controllers
         }
 
         [HttpGet]
+        [Authorize(Roles = "1")]
         public async Task<IActionResult> Obtener()
         {
             var resultado = await _vehiculoFlujo.Obtener();
@@ -51,6 +57,7 @@ namespace API.Controllers
         }
 
         [HttpGet("{Id}")]
+        [Authorize(Roles = "1")]
         public async Task<IActionResult> Obtener([FromRoute]Guid Id)
         {
             var resultado = await _vehiculoFlujo.Obtener(Id);
--- Vehiculo.API/API/Program.cs
+++ Vehiculo.API/API/Program.cs
@@ -2,13 +2,35 @@ using Abstracciones.Interfaces.DA;
 using Abstracciones.Interfaces.Flujo;
 using Abstracciones.Interfaces.Reglas;
 using Abstracciones.Interfaces.Servicios;
+using Abstracciones.Modelos;
 using DA;
 using DA.Repositorios;
 using Flujo;
 using Reglas;
 using Servicios;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Autorizacion.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var tokenConfig = builder.Configuration.GetSection("Token").Get<TokenConfiguracion>();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = tokenConfig.Issuer,
+            ValidAudience = tokenConfig.Audience,
+            IssuerSigningKey = new SymmetricSecurityKey(
+                                           Encoding.UTF8.GetBytes(tokenConfig.key))
+        };
+    });
 
 // Add services to the container.
 
@@ -28,6 +50,13 @@ builder.Services.AddScoped<IConfiguracion, Configuracion>();
 builder.Services.AddScoped<IRegistroReglas, RegistroReglas>();
 builder.Services.AddScoped<IRevisionReglas, RevisionReglas>();
 
+builder.Services.AddTransient<Autorizacion.Abstracciones.Flujo.IAutorizacionFlujo,
+                               Autorizacion.Flujo.AutorizacionFlujo>();
+builder.Services.AddTransient<Autorizacion.Abstracciones.DA.ISeguridadDA,
+                               Autorizacion.DA.SeguridadDA>();
+builder.Services.AddTransient<Autorizacion.Abstracciones.DA.IRepositorioDapper,
+                               Autorizacion.DA.Repositorios.RepositorioDapper>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -39,6 +68,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.AutorizacionClaims();
 app.UseAuthorization();
 
 app.MapControllers();
